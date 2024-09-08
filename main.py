b4ffa26f417a5a88d436a179f794887b7be43cfb
Cameron West <camonater62@gmail.com>
2024-09-08 08:02:00 -0700

    Get thumbnail and title information

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import urllib.parse
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 import os
 import validators
 import sqlite3
 import urllib
+import yt_dlp
 
 sqliteConnection = sqlite3.connect('sql.db')
 cursor = sqliteConnection.cursor()
@@ -16,7 +17,7 @@ result = cursor.fetchall()
 print(f"SQLite Version is {result}")
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS videos
-               (url TEXT, votes integer)''')
+               (url TEXT, votes integer, title TEXT, thumbnail TEXT)''')
 
 cursor.close()
 sqliteConnection.commit()
@@ -32,15 +33,18 @@ def ping():
 @app.route("/recommended", methods=['GET'])
 def recommended():
     print(request)
+    results = []
     try:
         connection = sqlite3.connect("sql.db")
-        cursor = connection.execute("SELECT * from videos")
-        for row in cursor:
-            print(row)
+        cursor = connection.execute("""SELECT * 
+                                    FROM videos 
+                                    ORDER BY votes DESC 
+                                    LIMIT 5""")
+        results = cursor.fetchall()
     finally:
         connection.close()
         
-    return Response(status=200)
+    return jsonify(results)
 
 @app.route("/vote/<url>", methods=['POST'])
 def vote(url):
@@ -53,7 +57,13 @@ def vote(url):
             existing = cursor.fetchone()
             print(existing)
             if existing == None:
-                cursor.execute("""INSERT INTO videos VALUES (?, ?)""", (url, 1))
+                with yt_dlp.YoutubeDL() as ydl:
+                    info_dict = ydl.extract_info(full_url, download=False)
+                    # print(info_dict.keys())
+                    title = info_dict['title']
+                    thumbnail = info_dict['thumbnail']
+                cursor.execute("""INSERT INTO videos VALUES (?, ?, ?, ?)""", 
+                               (url, 1, title, thumbnail))
             else:
                 cursor.execute("UPDATE videos SET votes=? WHERE url=?", (existing[1] + 1, url))
             connection.commit()
